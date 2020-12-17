2475f6f17461f1698601ea8de29437352764a4d5
小滕 <xiaoteng@192.168.0.103>
2020-12-17 20:26:16 +0800

    电子书插件弹窗优化

--- src/components/extentions/meeduBooks/article_comment/index.vue
+++ src/components/extentions/meeduBooks/article_comment/index.vue
@@ -2,6 +2,9 @@
   <div class="h-panel w-1200">
     <div class="h-panel-bar">
       <span class="h-panel-title">评论</span>
+      <div class="h-panel-right">
+        <Button @click="$emit('close')" :text="true">取消</Button>
+      </div>
     </div>
     <div class="h-panel-body">
       <div class="float-box mb-10">
@@ -58,15 +61,7 @@
       </div>
 
       <div class="float-box mb-10">
-        <Pagination
-          class="mt-10"
-          v-if="pagination.total > 0"
-          align="right"
-          :size="pagination.size"
-          :cur="pagination.page"
-          :total="pagination.total"
-          @change="changePage"
-        />
+        <Pagination class="mt-10" align="right" :size="pagination.size" :cur="pagination.page" :total="pagination.total" @change="changePage" />
       </div>
     </div>
   </div>
