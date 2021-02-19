174e9585c617ebfa2be03f50a91485226686e8a1
小滕 <xiaoteng@192.168.0.103>
2021-02-19 22:58:31 +0800

    优化wangEditor

--- src/components/common/wangEditor.vue
+++ src/components/common/wangEditor.vue
@@ -41,23 +41,23 @@ export default {
   },
   methods: {
     seteditor() {
-      this.editor = new E('#' + this.editorId);
+      const editor = new E('#' + this.editorId);
 
-      this.editor.config.zIndex = 1;
-      this.editor.config.height = this.height;
-      this.editor.config.placeholder = '请输入内容';
-      this.editor.config.uploadImgShowBase64 = false; // base 64 存储图片
-      this.editor.config.uploadImgServer = '/backend/api/v1/upload/image/tinymce'; // 配置服务器端地址
-      this.editor.config.uploadImgHeaders = {
+      editor.config.zIndex = 1;
+      editor.config.height = this.height;
+      editor.config.placeholder = '请输入内容';
+      editor.config.uploadImgShowBase64 = false; // base 64 存储图片
+      editor.config.uploadImgServer = '/backend/api/v1/upload/image/tinymce'; // 配置服务器端地址
+      editor.config.uploadImgHeaders = {
         Authorization: 'Bearer ' + Utils.getLocal('token')
       }; // 自定义 header
-      this.editor.config.uploadFileName = 'file'; // 后端接受上传文件的参数名
-      this.editor.config.uploadImgMaxSize = 5 * 1024 * 1024; // 将图片大小限制为 2M
-      this.editor.config.uploadImgMaxLength = 1; // 限制一次最多上传 3 张图片
-      this.editor.config.uploadImgTimeout = 60 * 1000; // 设置超时时间
+      editor.config.uploadFileName = 'file'; // 后端接受上传文件的参数名
+      editor.config.uploadImgMaxSize = 5 * 1024 * 1024; // 将图片大小限制为 2M
+      editor.config.uploadImgMaxLength = 1; // 限制一次最多上传 3 张图片
+      editor.config.uploadImgTimeout = 60 * 1000; // 设置超时时间
 
       // 配置菜单
-      this.editor.config.menus = [
+      editor.config.menus = [
         'head', // 标题
         'bold', // 粗体
         'fontSize', // 字号
@@ -74,19 +74,21 @@ export default {
         'code' // 插入代码
       ];
 
-      this.editor.config.uploadImgHooks = {
+      editor.config.uploadImgHooks = {
         customInsert: (insertImg, result) => {
           var url = result.location;
           insertImg(url);
         }
       };
-      this.editor.config.onchange = html => {
+      editor.config.onchange = html => {
         this.info_ = html;
         this.$emit('change', this.info_);
       };
-      this.editor.create();
+      editor.create();
 
-      this.editor.txt.html(this.value);
+      editor.txt.html(this.value);
+
+      this.editor = editor;
     }
   }
 };
