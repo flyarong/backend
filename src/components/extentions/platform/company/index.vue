e39ca6e5d342a7fb633441f9e16f5c89c7c45390
小滕 <xiaoteng@192.168.0.103>
2021-03-10 11:31:19 +0800

    机构余额

--- src/components/extentions/platform/company/index.vue
+++ src/components/extentions/platform/company/index.vue
@@ -11,6 +11,9 @@
         <Table :loading="loading" :datas="datas">
           <TableItem prop="id" title="机构ID" :width="100"></TableItem>
           <TableItem prop="name" title="机构名" :width="300"></TableItem>
+          <TableItem title="账户余额" :width="150">
+            <template slot-scope="{ data }">{{ data.balance / 100 }}元</template>
+          </TableItem>
           <TableItem title="管理员" :width="150">
             <template slot-scope="{ data }">{{ data.admin_user.nickname }}</template>
           </TableItem>
@@ -25,7 +28,6 @@
           </TableItem>
         </Table>
       </div>
-
       <div class="float-box mb-10">
         <Pagination class="mt-10" align="right" v-model="pagination" @change="changePage" />
       </div>
